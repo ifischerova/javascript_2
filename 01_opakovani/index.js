da07a7104dd4f8036e94813ec127240134f56137
Ivuska <fischerova.ivka@gmail.com>
2023-02-17 09:29:31 +0100

    First excercise solution.

---
+++ 01_opakovani/index.js
@@ -0,0 +1,7 @@
+const rodneCislo = Number(prompt('Zadej sve rodne cislo:'));
+
+if (rodneCislo.length !== 10) {
+    console.log("Uživatel zadal rodné číslo neplatné délky.");
+} else {
+    'Zadané rodné číslo má správně deset znaků.';
+}
