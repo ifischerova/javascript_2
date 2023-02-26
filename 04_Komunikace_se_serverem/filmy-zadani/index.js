6b622274f809a4a35b64ebbd67a744880fa89350
Ivuska <fischerova.ivka@gmail.com>
2023-02-26 16:41:45 +0100

    Reseni Zobrazovani dat - filmy podle zanru.

--- 04_Komunikace_se_serverem/filmy-zadani/index.js
+++ 04_Komunikace_se_serverem/filmy-zadani/index.js
@@ -3,7 +3,7 @@ console.log('funguju!');
 // Zobrazovani dat
 //1. Filmy
 
-fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+/*fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
     .then((response) => {
         return response.json();
     })
@@ -28,6 +28,57 @@ fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
                 </li>`
         });
     });
+*/
+
+// 2. Filmy dle zanru
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/genres')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        const selectOption = document.querySelector('#select-genre')
+        selectOption.innerHTML = data.map((genre) => {
+            return `
+            <option value=${genre}>${genre}</option>
+            `
+        })
+    })
+
+const renderFilms = (movies) => {
+    const movieDetail = document.querySelector('.movie-list');
+    movieDetail.innerHTML = movies.map((movie) => {
+        return `
+            <li class="movie-detail">
+                <div class="movie-poster">
+                <img 
+                    src=${movie.posterUrl}
+                    alt=${movie.title}
+                />
+                </div>
+                <div class="movie-info">
+                <h2 class="movie-title">${movie.title}</h2>
+                <div class="movie-year">Rok vydání: ${movie.year}</div>
+                <div class="movie-link">
+                     <a href=${movie.link} target="_blank">Odkaz na CSFD</a>
+                </div>
+                </div>
+            </li>`
+    });
+};
+
+
+const filterForm = document.querySelector('.filters');
+filterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const selectElm = document.querySelector('#select-genre');
+    fetch(`https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies?genre=${selectElm.value}`)
+        .then((response) => {
+            return response.json();
+        
+        })
+        .then(renderFilms);
+});
 
 
 
