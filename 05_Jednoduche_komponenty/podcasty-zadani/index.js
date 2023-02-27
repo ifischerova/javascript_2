b6dc4dc418e0cb054e2b1510d771dbd18a05c484
Ivuska <fischerova.ivka@gmail.com>
2023-02-27 11:03:40 +0100

    Reseni Doporucene ulzky na doma - podcasty.

---
+++ 05_Jednoduche_komponenty/podcasty-zadani/index.js
@@ -0,0 +1,48 @@
+import { Podcast } from '../Podcast/index.js';
+
+const episodes = [
+  {
+    num: 127,
+    title: 'Hledání plyšového Yettiho',
+    guest: 'Vojtěch Ryba',
+  },
+  {
+    num: 128,
+    title: 'Moderní hrachová polévka',
+    guest: 'Kamila Štancová',
+  },
+  {
+    num: 129,
+    title: 'Poloautomatické zrcadlo',
+    guest: 'Janka Janovská',
+  },
+  {
+    num: 130,
+    title: 'Máčené hlavy parlamentu',
+    guest: 'Jonáš Daněk',
+  },
+  {
+    num: 131,
+    title: 'Služby na hraně přívěsu',
+    guest: 'Tereza Křivánková',
+  },
+  {
+    num: 132,
+    title: 'Klasifikace sněžných klokanů',
+    guest: 'Josef Stix',
+  },
+  {
+    num: 133,
+    title: 'Rybolov v Oceánu bouří',
+    guest: 'Ludmila Gajová',
+  },
+];
+
+console.log('funguju!', episodes);
+
+const renderPodcasts = (episodes) => {
+  const podcastList = document.querySelector('.episodes-list');
+  podcastList.innerHTML = episodes.map((episode) => Podcast({num: episode.num, title: episode.title, guest: episode.guest })).join('');
+};
+
+renderPodcasts(episodes);
